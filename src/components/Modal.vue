f2429f869c1e10e942e6c6dc3cb8e958b6997747
North-Wang <103781180+North-Wang@users.noreply.github.com>
2023-11-01 18:02:24 +0800

    init

---
+++ src/components/Modal.vue
@@ -0,0 +1,25 @@
+<template>
+  <template ref="target">
+    <div id="header" style="width: 100%"></div>
+    <slot name="body"> 彈窗內容 </slot>
+    <slot name="footer"></slot>
+  </template>
+</template>
+
+<script setup>
+import { ref, onMounted, watch, defineEmits } from "vue";
+import { onClickOutside } from "@vueuse/core";
+
+const emits = defineEmits(["closeModal"]);
+
+const target = ref(null);
+onClickOutside(target, () => {
+  closeModal();
+});
+
+const closeModal = () => {
+  emits("closeModal");
+};
+</script>
+
+<style scoped></style>
