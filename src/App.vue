553bafdffd6be6bce02603ca90eb041fe684faf6
North-Wang <103781180+North-Wang@users.noreply.github.com>
2023-11-02 15:53:31 +0800

    引入Tailwind.css、彈窗模組化

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,44 @@
+<template>
+  <div class="flex gap-x-1">
+    <button @click="showBasicModal = true">打開基本文字彈窗</button>
+    <button @click="showModal = true">打開彈窗</button>
+  </div>
+
+  <Teleport to="body" v-if="showBasicModal">
+    <Modal :width="'398px'" @closeModal="showBasicModal = false">
+      <template #modalContent> </template>
+    </Modal>
+  </Teleport>
+  <Teleport to="body" v-if="showModal">
+    <Modal :width="'398px'" :hasHeader="true" @closeModal="showModal = false">
+      <template #modalContent>
+        <div class="text-[18px]">自動化劇本提醒</div>
+        <br />
+        <div class="text-[14px]">
+          <li class="list-align">
+            需先啟用ReMA功能（Email&手機號碼授權、AWS認證、網域驗證、客戶教育訓練）
+          </li>
+          <li class="list-align">需先確認觸發管道，並確認已完成串接</li>
+          <li class="list-align">
+            若測試過程中有任何疑問或需求，煩請與負責窗口聯繫
+          </li>
+          <li class="list-align">
+            劇本中所有寄出信件不屬於系統信，將會受到「行銷名單狀態」的限制，因此信件內容建議放入取消訂閱。
+          </li>
+        </div>
+        <!-- <div>發送成功!</div> -->
+      </template>
+    </Modal>
+  </Teleport>
+</template>
+
 <script setup>
 import { ref, Teleport } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
 import Modal from "./components/Modal.vue";
 
 const showModal = ref(false);
+const showBasicModal = ref(false);
 </script>
-
-<template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <button @click="showModal = true">打開彈窗</button>
-  <HelloWorld msg="Vite + Vue" />
-  <Teleport to="body" v-if="showModal">
-    <Modal @closeModal="showModal = false"></Modal>
-  </Teleport>
-</template>
 
 <style scoped>
 .logo {
@@ -34,5 +52,10 @@ const showModal = ref(false);
 }
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
+}
+.list-align {
+  list-style-type: disc;
+  list-style-position: outside;
+  margin-left: 25px;
 }
 </style>
--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,17 +1,90 @@
 <template>
-  <div class="modal-bg">
-    <div class="modal-content" ref="target">
-      <div id="header" style="width: 100%"></div>
-      <slot name="body"> 彈窗內容 </slot>
-      <slot name="footer"></slot>
+  <transition name="modal">
+    <div class="modal-bg">
+      <ul
+        class="modal-content rounded-lg overflow-hidden"
+        ref="target"
+        :style="{ width: width, height: height, minHeight: minHeight }"
+      >
+        <li class="header" v-show="hasHeader">
+          <slot name="headerTitle">&nbsp;</slot>
+          <img
+            :src="Xmark"
+            alt="close-modal"
+            class="w-2.5 h-2.5 cursor-pointer"
+            @click.self="closeModal"
+          />
+        </li>
+        <!-- 內文 -->
+        <li class="body p-4">
+          <slot name="modalContent"></slot>
+        </li>
+        <!-- modal footer -->
+
+        <li
+          class="footer flex justify-end px-4 pb-4 gap-[24px]"
+          v-show="hasFooter"
+        >
+          <slot name="footer"></slot>
+          <button class="basic-button-light" @click="closeModal">
+            <slot name="cancel ">取消</slot>
+          </button>
+          <button class="basic-button" @click="closeModal">
+            <slot name="closeModal ">確認</slot>
+          </button>
+        </li>
+      </ul>
     </div>
-  </div>
+  </transition>
 </template>
 
 <script setup>
-import { ref, onMounted, watch, defineEmits } from "vue";
+import { ref, onMounted, watch, computed, defineEmits, defineProps } from "vue";
 import { onClickOutside } from "@vueuse/core";
+import Xmark from "../assets/closeModal.svg";
 
+const props = defineProps({
+  width: {
+    //彈窗寬度
+    type: String,
+    default: "100%",
+  },
+  height: {
+    //彈窗高度
+    type: String,
+    default: "",
+  },
+  minHeight: {
+    //彈窗最小高度
+    type: String,
+    default: "",
+  },
+  hasHeader: {
+    //是否要有header
+    type: Boolean,
+    default: true,
+  },
+  hasFooter: {
+    //是否要有footer
+    type: Boolean,
+    default: true,
+  },
+});
+const width = computed(() => {
+  return props.width;
+});
+const height = computed(() => {
+  return props.height;
+});
+const minHeight = computed(() => {
+  return props.minHeight;
+});
+const hasHeader = computed(() => {
+  return props.hasHeader;
+});
+const hasFooter = computed(() => {
+  return props.hasFooter;
+});
 const emits = defineEmits(["closeModal"]);
 
 const target = ref(null);
@@ -36,9 +109,16 @@ const closeModal = () => {
   align-items: center;
 }
 .modal-content {
-  width: 100px;
-  height: 500px;
+  width: 100%;
   background-color: white;
   opacity: 1;
+}
+.header {
+  height: 40px;
+  background: var(--unnamed, linear-gradient(90deg, #b5dbe0 0%, #70aeb5 100%));
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 9px 15px;
 }
 </style>
