d2ec702d4c250cd9d3f53efbca5911141d075267
North-Wang <103781180+North-Wang@users.noreply.github.com>
2023-11-09 10:53:13 +0800

    fix:調整模組設計

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,200 @@
 <template>
-  <div class="flex gap-x-1">
-    <button @click="showBasicModal = true">基本文字彈窗</button>
-    <button @click="showModal = true">劇本提醒</button>
-  </div>
+  <ul class="flex flex-col gap-y-2">
+    <li class="flex gap-x-1">
+      <button @click="showModal = true">預設</button>
+    </li>
+    <hr />
+    <li class="flex gap-x-1">
+      <button @click="showBasicModal = true">有彈窗標題</button>
 
+      <button @click="showTwoButton = true">兩個按鈕</button>
+      <button @click="showFourthButton = true">四個按鈕</button>
+      <button @click="showNoButton = true">沒有任何按鈕</button>
+    </li>
+    <hr />
+    <h3 class="text-start">footer的樣式</h3>
+    <li class="flex gap-x-1">
+      <button @click="showJustifyStart = true">靠左對齊</button>
+      <button @click="showJustifyBetween = true">兩側對齊</button>
+      <button @click="showJustifyEnd = true">靠右對齊</button>
+    </li>
+    <li class="flex gap-x-1">
+      <button @click="showOneLeftTwoRight = true">左一右二按鈕</button>
+    </li>
+    <hr />
+    <h3 class="text-start">範例</h3>
+  </ul>
+
+  <Teleport to="body">
+    <Modal
+      :width="'398px'"
+      @closeModal="showModal = false"
+      @clickButton1="showModal = false"
+      v-if="showModal"
+    >
+    </Modal>
+  </Teleport>
   <Teleport to="body" v-if="showBasicModal">
-    <Modal :width="'398px'" @closeModal="showBasicModal = false">
+    <Modal
+      :width="'398px'"
+      @closeModal="showBasicModal = false"
+      @clickButton1="showBasicModal = false"
+    >
+      <template #headerTitle>
+        <div>彈窗的標題</div>
+      </template>
       <template #modalContent>
         <div class="text-[#575757]">發送成功!</div>
       </template>
-      <template #close-button>確認</template>
+      <template #button1>確認</template>
     </Modal>
   </Teleport>
-  <Teleport to="body" v-if="showModal">
-    <Modal :width="'398px'" :hasHeader="true" @closeModal="showModal = false">
+  <Teleport to="body" v-if="showTwoButton">
+    <Modal
+      :width="'398px'"
+      :showButton2="true"
+      @closeModal="showTwoButton = false"
+      @clickButton1="testButton1"
+      @clickButton2="testButton2"
+    >
       <template #modalContent>
-        <div class="text-[18px]">提醒</div>
-        <br />
-        <div class="text-[14px]">
-          <li class="list-align">需先確認觸發管道，並確認已完成串接</li>
+        <ul>
+          <li class="font-[18px] text-sky-400">劇本測試方法</li>
           <li class="list-align">
-            若測試過程中有任何疑問或需求，煩請與負責窗口聯繫
+            若您要測試註冊事件，請在此輸入您即將在購物網站要註冊的信箱或電話，該劇本會僅針對您所輸入的信箱做啟用(其他消費者不會被觸發)。
           </li>
-          <li class="flex text-[18px] mt-5 leading-4 items-center font-bold">
-            <input
-              type="checkbox"
-              value="closeModal"
-              id="closeModal"
-              v-model="notShowModal"
-              class="cursor-pointer w-5 h-5"
-              @input="changeInput"
-            />
-            <label for="closeModal" class="cursor-pointer ml-2"
-              >今日不再顯示本視窗。</label
-            >
+          <li class="list-align">
+            若您要測試註冊以外的事件，請在此輸入您購物網站已經是會員的信箱或手機，該劇本會僅針對您所輸入的信箱做啟用(其他消費者不會被觸發)。
           </li>
-        </div>
-        <!-- <div>發送成功!</div> -->
+        </ul>
       </template>
+      <template #button1>第一個按鈕</template>
+      <template #button2>第二個按鈕</template>
+    </Modal>
+  </Teleport>
+  <Teleport to="body" v-if="showFourthButton">
+    <Modal
+      :width="'600px'"
+      :showButton2="true"
+      :showButton3="true"
+      :showButton4="true"
+      :setClassButton1="'basic-button'"
+      :setClassButton2="'basic-button-light'"
+      :setClassButton3="'basic-button'"
+      @closeModal="showFourthButton = false"
+      @clickButton1="testButton1"
+      @clickButton2="testButton2"
+      @clickButton3="testButton3"
+      @clickButton4="testButton4"
+    >
+      <template #modalContent>
+        <div style="min-height: 50px">
+          確認後仍可編輯投放設定，但寄出前10分鐘內將無法進行任何更改。
+        </div>
+      </template>
+      <template #button1>按鈕1</template>
+      <template #button2> 按鈕2 </template>
+      <template #button3>按鈕3</template>
+      <template #button4>按鈕4</template>
+    </Modal>
+  </Teleport>
+  <Teleport to="body" v-if="showNoButton">
+    <Modal
+      :width="'500px'"
+      :hasFooter="false"
+      @closeModal="showNoButton = false"
+    >
+      <template #modalContent>19:00 君悅酒店柏麗廳</template>
+    </Modal>
+  </Teleport>
+  <Teleport to="body" v-if="showJustifyStart">
+    <Modal
+      :width="'500px'"
+      :showButton2="true"
+      :showButton3="true"
+      :footerClass="'justify-start'"
+      :setClassButton2="'basic-button-light'"
+      @closeModal="showJustifyStart = false"
+      @clickButton1="showJustifyStart = false"
+      @clickButton2="showJustifyStart = false"
+    >
+      <template #modalContent>已收到信件</template>
+    </Modal>
+  </Teleport>
+  <Teleport to="body" v-if="showJustifyBetween">
+    <Modal
+      :width="'700px'"
+      :showButton2="true"
+      :showButton3="true"
+      :showButton4="true"
+      :footerClass="'justify-between'"
+      :setClassButton2="'basic-button-light'"
+      @closeModal="showJustifyBetween = false"
+      @clickButton1="showJustifyBetween = false"
+      @clickButton2="showJustifyBetween = false"
+      @clickButton3="showJustifyBetween = false"
+      @clickButton4="showJustifyBetween = false"
+    >
+      <template #modalContent>已收到信件</template>
+      <template #button1>按鈕一</template>
+      <template #button2>按鈕二</template>
+      <template #button3></template>
+      <template #button4></template>
+    </Modal>
+  </Teleport>
+  <Teleport to="body" v-if="showJustifyEnd">
+    <Modal
+      :width="'500px'"
+      :showButton2="true"
+      :showButton3="true"
+      :setClassButton2="'basic-button-light'"
+      @closeModal="showJustifyEnd = false"
+      @clickButton1="showJustifyEnd = false"
+      @clickButton2="showJustifyEnd = false"
+    >
+      <template #modalContent>已收到信件</template>
+    </Modal>
+  </Teleport>
+  <Teleport to="body" v-if="showOneLeftTwoRight">
+    <Modal
+      :width="'500px'"
+      :showButton2="true"
+      :showButton3="true"
+      :footerClass="'justify-between'"
+      @closeModal="showOneLeftTwoRight = false"
+      @clickButton1="showOneLeftTwoRight = false"
+      @clickButton2="showOneLeftTwoRight = false"
+      @clickButton3="showOneLeftTwoRight = false"
+    >
+      <template #headerTitle>更改合約內容</template>
+      <template #modalContent>請確認內容正確無誤之後送出</template>
+      <template #button1>送出</template>
+      <template #button3>重新設定</template>
     </Modal>
   </Teleport>
 </template>
 
 <script setup>
-import { ref, Teleport } from "vue";
+import { ref, Teleport, onMounted } from "vue";
 import Modal from "./components/Modal.vue";
+import moment from "moment";
 
 const showModal = ref(false);
 const showBasicModal = ref(false);
+const showTwoButton = ref(false);
+const showFourthButton = ref(false);
+const showNoButton = ref(false);
+const showJustifyStart = ref(false);
+const showJustifyBetween = ref(false);
+const showJustifyEnd = ref(false);
+const showOneLeftTwoRight = ref(false);
 
-const notShowModal = ref(false);
+const testButton1 = () => [alert("自訂的第一個按鈕事件")];
+const testButton2 = () => [alert("自訂的第二個按鈕事件")];
+const testButton3 = () => [alert("自訂的第三個按鈕事件")];
+const testButton4 = () => [alert("自訂的第四個按鈕事件")];
 
-const changeInput = () => {
-  console.log("點擊input");
-};
+onMounted(() => {});
 </script>
 
 <style scoped>
--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,113 +1,148 @@
 <template>
-  <transition name="modal">
-    <div class="modal-bg">
+  <div class="modal-bg">
+    <transition name="slide-fade">
       <ul
         class="modal-content rounded-lg overflow-hidden"
         ref="target"
-        :style="{ width: width, height: height, minHeight: minHeight }"
+        :style="setModalStyle"
         role="dialog"
         aria-labelledby="modalTitle"
         aria-describedby="modalDescription"
+        v-show="showModal"
       >
         <li class="header" v-show="hasHeader">
-          <slot name="headerTitle">&nbsp;</slot>
+          <div class="flex">
+            <img
+              :src="goBackIcon"
+              alt="go-back-icon"
+              class="w-2.5 h-2.5 cursor-pointer"
+              @click.self="closeModal"
+              v-show="hasBackIcon"
+            />
+            <div class="text-white" :style="setHeaderTitleStyle">
+              <slot name="headerTitle "></slot>
+            </div>
+          </div>
+
           <img
             :src="Xmark"
             alt="close-modal"
             class="w-2.5 h-2.5 cursor-pointer"
             @click.self="closeModal"
+            v-show="hasXMark"
           />
         </li>
-        <!-- 內文 -->
-        <li class="body p-4">
+        <!-- body -->
+        <li class="body flex flex-col" :style="setBodyStyle">
           <slot name="modalContent"></slot>
+          <!-- default button -->
+          <slot name="body-bottom">
+            <div :style="setBodyBottom" v-if="hasBodyButtonList">
+              <button
+                class="button-basic"
+                :style="setButtonStyle"
+                @click="clickButton"
+              >
+                確認
+              </button>
+            </div>
+          </slot>
         </li>
-        <!-- 按鈕 -->
-        <li
-          class="footer flex justify-end px-4 pb-4 gap-[24px]"
-          v-show="hasFooter"
-        >
-          <button
-            class="basic-button-light"
-            @click="closeModal"
-            v-show="showLightButton"
-          >
-            <slot name="button-light">取消</slot>
-          </button>
-          <button class="basic-button" @click="closeModal">
-            <slot name="close-button">確認</slot>
-          </button>
+        <!-- footer -->
+        <li class="footer" v-if="hasFooter">
+          <slot name="footer">
+            <div class="footer-default-content">footer default content</div>
+          </slot>
         </li>
       </ul>
-    </div>
-  </transition>
+    </transition>
+  </div>
 </template>
 
 <script setup>
 import { ref, onMounted, watch, computed, defineEmits, defineProps } from "vue";
 import { onClickOutside } from "@vueuse/core";
 import Xmark from "../assets/closeModal.svg";
+import goBackIcon from "../assets/goBackIcon.svg";
 
 const props = defineProps({
-  width: {
-    //彈窗寬度
-    type: String,
-    default: "100%",
-  },
-  height: {
-    //彈窗高度
-    type: String,
-    default: "",
-  },
-  minHeight: {
-    //彈窗最小高度
-    type: String,
-    default: "",
-  },
+  /* ----- 與元件顯示相關 start ----- */
   hasHeader: {
-    //是否要有header
+    type: Boolean,
+    default: true,
+  },
+  hasBackIcon: {
+    //標頭左側的關閉彈窗icon
+    type: Boolean,
+    default: false,
+  },
+  hasXMark: {
+    //標頭右側的關閉彈窗icon
     type: Boolean,
     default: true,
   },
   hasFooter: {
-    //是否要有footer
-    type: Boolean,
-    default: true,
-  },
-  showLightButton: {
-    //是否顯示淺色按鈕
     type: Boolean,
     default: false,
   },
-});
-const width = computed(() => {
-  return props.width;
-});
-const height = computed(() => {
-  return props.height;
-});
-const minHeight = computed(() => {
-  return props.minHeight;
-});
-const hasHeader = computed(() => {
-  return props.hasHeader;
-});
-const hasFooter = computed(() => {
-  return props.hasFooter;
-});
-const showLightButton = computed(() => {
-  return props.showLightButton;
+  hasBodyButtonList: {
+    //是否顯示body下方的按鈕列
+    type: Boolean,
+    default: true,
+  },
+  /* ----- 與元件顯示相關 end ----- */
+
+  /* ----- 修改style start ----- */
+  //彈窗本身
+  setModalStyle: {
+    type: Object,
+    default: () => ({
+      width: "50%",
+      minHeight: "100px",
+    }),
+  },
+  //標頭的標題
+  setHeaderTitleStyle: {
+    type: Object,
+    default: () => ({}),
+  },
+  setBodyStyle: {
+    type: Object,
+    default: () => ({
+      padding: "1rem",
+    }),
+  },
+  setButtonStyle: {
+    type: Object,
+    default: () => ({
+      justifyContent: "end",
+      marginTop: "25px",
+      display: "flex",
+    }),
+  },
+  /* ----- 修改style end ----- */
 });
 const emits = defineEmits(["closeModal"]);
 
 const target = ref(null);
-onClickOutside(target, () => {
+onClickOutside(target, (e) => {
   closeModal();
 });
-
+const showModal = ref(false);
 const closeModal = () => {
-  emits("closeModal");
+  //關閉彈窗
+  showModal.value = false;
+  setTimeout(() => {
+    emits("closeModal");
+  }, 400);
 };
+const clickButton = () => {
+  closeModal;
+};
+
+onMounted(() => {
+  showModal.value = true;
+});
 </script>
 
 <style scoped>
@@ -120,18 +155,45 @@ const closeModal = () => {
   display: flex;
   justify-content: center;
   align-items: center;
+  font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
 }
 .modal-content {
   width: 100%;
   background-color: white;
   opacity: 1;
+  display: flex;
+  flex-direction: column;
 }
 .header {
-  height: 40px;
+  height: 30px;
   background: var(--unnamed, linear-gradient(90deg, #b5dbe0 0%, #70aeb5 100%));
   display: flex;
   justify-content: space-between;
   align-items: center;
   padding: 9px 15px;
+}
+.footer-default-content {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  justify-content: center;
+  text-align: center;
+  padding: 12px;
+}
+/* 進場動畫 */
+.slide-fade-enter-active {
+  transition: all 0.3s ease-out;
+  transition-delay: 0.1s;
+}
+
+.slide-fade-leave-active {
+  transition: all 0.2s linear;
+  transition-delay: 0.1s;
+}
+
+.slide-fade-enter-from,
+.slide-fade-leave-to {
+  transform: translateY(-30px);
+  opacity: 0;
 }
 </style>
